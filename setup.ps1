aa7193b93ffe37c964063fda87d0ef62ccc0a42e
regularpooria <pooriababak444@gmail.com>
2026-02-07 18:11:28 -0500

    fixed audio ffmpeg

--- setup.ps1
+++ setup.ps1
@@ -5,4 +5,4 @@ virtualenv venv
 pip install -r requirements.txt
 $env:PLAYWRIGHT_BROWSERS_PATH="0"
 playwright install chromium
-pyinstaller --noconfirm --onefile --windowed --icon "favicon.ico" --name "Willow" --add-data "records:records/" --add-data "public:public/" --add-data "web:web/" --add-data ".env:."  "web.py"
+pyinstaller --noconfirm --onefile --windowed --icon "favicon.ico" --name "Willow" --add-data "records:records/" --add-data "public:public/" --add-data "web:web/" --add-data ".env:." --exclude-module PyQt6.QtQuick3D "web.py"
--- transcript_to_audio.py
+++ transcript_to_audio.py
@@ -1,22 +1,40 @@
 from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
-
+from playsound3 import playsound
 import os
+import sys
+
+load_dotenv()
+
+client = ElevenLabs(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
+)
+
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 
 def trans_to_aud(transcript):
-    load_dotenv()
 
-    elevenlabs = ElevenLabs(
-        api_key=os.getenv("ELEVENLABS_API_KEY"),
-    )
-
-    audio = elevenlabs.text_to_speech.convert(
+    audio_stream = client.text_to_speech.convert(
         text=transcript,
         voice_id="JBFqnCBsd6RMkjVDRZzb",
         model_id="eleven_multilingual_v2",
         output_format="mp3_44100_128",
     )
 
-    play(audio)
+    # Write to a real file (PyInstaller-safe)
+    output_path = resource_path("output.mp3")
+
+    with open(output_path, "wb") as f:
+        for chunk in audio_stream:
+            f.write(chunk)
+
+    playsound(output_path)
